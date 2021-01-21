a236e04a202c138f693f0b1dca0fb3ca123e8721
NMShihab <nmshihabislam@gmail.com>
2021-01-21 14:11:47 +0600

    button header

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({title}) => {
+    const onClick = () =>{
+        console.log("Click")
+    }
+    return (
+        <header className = "header">
+            <h1>{title}</h1>   
+            <Button text = "Add" color = 'blue' onClick = {onClick}/>   
+                 
+
+            
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title : "Contact"
+}
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
+}
+
+// CSS in js 
+// const HeaderStyle = {
+//     color : 'blue',
+//     backgroundColor : 'red',
+
+// }
+
+export default Header
